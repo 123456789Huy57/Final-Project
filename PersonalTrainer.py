be8c1c3d6343a1f67d76862ef89ceebca95e9dc3
123456789Huy57 <quanghuylop910@gmail.com>
2025-03-21 21:16:16 +0700

    Add files via upload
    code and create all the models, test files

--- PersonalTrainer.py
+++ PersonalTrainer.py
@@ -1,11 +1,14 @@
-from Final.Models.User import User
+from FinalProject.models.User import User
+
 
 class PersonalTrainer(User):
-    def __init__(self, ID=None, First_Name=None, Last_Name=None, Gender=None, Username=None, Password=None, Experience=None, TypeofPt=None, Branch=None):
-        super().__init__(ID, First_Name, Last_Name, Gender, Username, Password)
+    def __init__(self, ID=None,Full_Name=None, Gender=None, Username=None, Password=None, Experience=None, TypeofPT=None, Branch=None,Email=None,Phone=None,RegisID=None):
+        super().__init__(ID,Full_Name, Gender, Username, Password)
         self.Experience = Experience
-        self.TypeofPT = TypeofPt
+        self.TypeofPT = TypeofPT
         self.Branch = Branch
-
+        self.Email=Email
+        self.Phone=Phone
+        self.RegisID = RegisID
     def __str__(self):
-        return f"{super().__str__()}\t{self.Experience}\t{self.TypeofPT}\t{self.Branch}"
+        return f"{super().__str__()}\t{self.Experience}\t{self.TypeofPT}\t{self.Branch}\t{self.Email}\t{self.Phone}\t{self.RegisID}"
--- User.py
+++ User.py
@@ -1,12 +1,9 @@
-
-
 class User:
-    def __init__(self,ID=None,First_Name=None,Last_Name=None,Gender=None,Username=None,Password=None):
+    def __init__(self,ID=None,Full_Name=None,Gender=None,Username=None,Password=None):
         self.ID=ID
-        self.First_Name=First_Name
-        self.Last_Name=Last_Name
+        self.Full_Name=Full_Name
         self.Gender=Gender
         self.Username=Username
         self.Password=Password
     def __str__(self):
-        return f"{self.ID}\t{self.First_Name}\t{self.Last_Name}\t{self.Gender}\t{self.Username}\t{self.Password}"
+        return f"{self.ID}\t{self.Full_Name}\t{self.Gender}\t{self.Username}\t{self.Password}"
